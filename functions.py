ff257951bcbc806dc9d0580031f021f88670e535
Savio <saviorabelo.ti@gmail.com>
2019-06-10 23:18:18 -0300

    Results Watershed

--- functions.py
+++ functions.py
@@ -1,20 +1,25 @@
 # Import library
 import matplotlib.pyplot as plt
 
-def show_images(image, markers, watershed):
-    fig, (ax0, ax1, ax2) = plt.subplots(nrows=1, ncols=3, figsize=(8, 2.5), sharex=True, sharey=True)
+def show_images(image, dilation, markers, watershed):
+    fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=4, figsize=(8, 2.5), sharex=True, sharey=True)
 
-    ax0.imshow(image, cmap=plt.get_cmap('gray'))
-    ax0.set_title('Image')
+    ax0.imshow(image)
+    ax0.set_title('Original')
     ax0.axis('off')
 
-    ax1.imshow(markers, cmap=plt.get_cmap('gray'))
-    ax1.set_title('Markers')
+    ax1.imshow(dilation, cmap=plt.get_cmap('gray'))
+    ax1.set_title('External Dilation')
     ax1.axis('off')
 
-    ax2.imshow(watershed, cmap=plt.get_cmap('nipy_spectral'), interpolation='nearest')
-    ax2.set_title('Watershed')
+    ax2.imshow(markers, cmap=plt.get_cmap('gray'))
+    ax2.set_title('Markers')
     ax2.axis('off')
+    
+    ax3.imshow(watershed, cmap=plt.get_cmap('nipy_spectral'), interpolation='nearest')
+    ax3.set_title('Watershed')
+    ax3.axis('off')
 
     fig.tight_layout()
     plt.show()
+# End
--- watershed.py
+++ watershed.py
@@ -1,9 +1,6 @@
 # coding: utf-8
 # Import library
 import numpy as np
-from scipy import misc, ndimage
-from scipy.ndimage import label
-from skimage import data
 from skimage.io import imread
 from skimage.color import rgb2gray
 from skimage import morphology
@@ -11,15 +8,16 @@ from skimage.morphology import disk, square, rectangle
 # Import functions
 from functions import *
 
-file_name = './Dataset Image/Alprazolam/4047_lg.jpg'
-image = rgb2gray(imread(file_name))
+file_name = './Dataset Image/Alprazolam/3527_lg.jpg'
+img = imread(file_name)
+image = rgb2gray(img)
 image_ext = morphology.dilation(image, disk(3)) - image
 
 # Watershed
 markers = np.zeros(image.shape)
-markers[90:110,20:40] = 200
-markers[90:110,90:110] = 100
+markers[40:60,20:40] = 200
+markers[90:110,90:110] = 100 # Center
 ws = morphology.watershed(image_ext, markers)
 
 # Plot
-show_images(255-image_ext, markers, ws)
+show_images(img, 255-image_ext, markers, ws)
