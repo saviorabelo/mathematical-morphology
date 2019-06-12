241f64ca8cb61ff7fff616e4d46d70a7d0907a8a
Savio <saviorabelo.ti@gmail.com>
2019-06-11 22:00:16 -0300

    Adjustment

--- watershed.py
+++ watershed.py
@@ -8,14 +8,15 @@ from skimage.morphology import disk, square, rectangle
 # Import functions
 from functions import *
 
-file_name = './Dataset Image/Alprazolam/3527_lg.jpg'
+#file_name = './Dataset Image/Alprazolam/3527_lg.jpg'
+file_name = './Dataset Image/Domino/4424_lg.jpg'
 img = imread(file_name)
 image = rgb2gray(img)
 image_ext = morphology.dilation(image, disk(3)) - image
 
 # Watershed
 markers = np.zeros(image.shape)
-markers[40:60,20:40] = 200
+markers[10:30,20:40] = 200
 markers[90:110,90:110] = 100 # Center
 ws = morphology.watershed(image_ext, markers)
 
